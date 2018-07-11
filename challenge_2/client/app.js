48d4cbd08ad6fa311849d69b75db9b5acb8eb45d
Ruixi <ruixi@Ruixis-MacBook-Pro.local>
2018-07-10 21:20:17 -0700

    sales figures not showing fixed

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,25 +1,11 @@
-/*
-Use Express to serve up an index.html file and its associated assets
-The client app should be able to submit JSON data to the server, receive a CSV-formatted response and then display the result on the same page.
-Implement all the report generation logic on the server. Do not use any external libraries (such as via npm).
-*/
-
-
 var app = {};
 
-// app.url = 'http://127.0.0.1:3000/'
+app.url = 'http://127.0.0.1:3000/'
 
 app.init = function(){
-	//to do at dom ready 
-	//fetch data
-
-	//if the button is clicked
-		//send ajax post call
-	
 	$(document).ready(function() {
 		$(".button").on("click", function(event) {
 			var inputData = $(".input").val()
-		  // console.log(inputData);
 			inputData = JSON.parse(inputData);
 			event.preventDefault();//prevent page from loading 
 		  	app.post(inputData);
@@ -29,16 +15,9 @@ app.init = function(){
 }
 
 app.post = function(data){//make Post request, ajax call
-	//success: display data
-	//fail: log 404
-	// data = JSON.parse(data);
-	// var cb = function(){
-	// 	console.log(serverData);
-	// };
-
 	$.ajax({
 	  type: "POST",
-	  url: 'http://127.0.0.1:3000/', 
+	  url: app.url, 
 	  data: data,
 	  success: function(newData){
 	  	console.log(newData);
@@ -46,21 +25,15 @@ app.post = function(data){//make Post request, ajax call
 
 	  },
 	  dataType: "json"
-	  // contentType : "application/json"
 	});
 }
 
 app.displayData = function(dataPost){
-//display incoming post data on the page
-
-	// dataPost.split(',').join()
-
 	var resultString = '';
 	dataPost.forEach((array)=>{
 		resultString += array.join(',') + '<br>';
 	})
 	$(".outputData").html(resultString);
-
 }
 
 app.init();
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
 
-// app.get('/', (req, res) => res.send('Hello World!'))
 app.use(function (req, res, next) {
   console.log(req.method,"- @", Date());
   next();
@@ -15,38 +14,35 @@ app.use(bodyParser.urlencoded({ extended: true }))//allow for next obj
 app.use(bodyParser.json())//google syntax 
 
 var destructor = function(obj){
-	var result = [];//create result array
+	//create result array
+	var result = [];
 	var temp = Object.keys(obj);
 	temp.pop();
 	result.push(temp);
 	var recursion = function(newObj){
-	    var temp = Object.values(newObj);
-	    temp.pop();
-	    result.push(temp);
-
-	    if(newObj.children){
-	      newObj.children.forEach((childObj) => {
-	        recursion(childObj);
-	      })
-	  	}
-	}
+	   var temp = [];
+	   for(var key in newObj){
+	       if(key !== 'children'){
+	           temp.push(newObj[key]);
+	       }
+	   }
+    result.push(temp);
+    if(newObj.children){
+      newObj.children.forEach((childObj) => {
+        recursion(childObj);
+      })
+    }
+    }
   recursion(obj);
   return result;
-};
+}
+
 
 app.post('/', function (req, res) {
 	res.status(201);
-	// var temp = destructor(JSON.parse(req.body));
 	var temp = destructor(req.body);
-	// console.log(req.body);
-
 	res.json(temp);
-	// var temp = JSON.parse(req.body);
-	// res.send(temp);
-	//handle err
 });
-
-
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
